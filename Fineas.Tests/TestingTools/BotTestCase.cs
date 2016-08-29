2f0033772a71ed9c2e92e5e362142a6853330340
Tess <tess.distefano@gmail.com>
2016-08-29 14:09:31 -0700

    Add Testing helpers from AzureBot #1

---
+++ Fineas.Tests/TestingTools/BotTestCase.cs
@@ -0,0 +1,28 @@
+﻿// I got this from AzureBot, author: Thiago Almeida
+// https://github.com/Microsoft/AzureBot/tree/master/AzureBot.Tests
+
+namespace Fineas.Tests.TestingTools
+{
+    using System;
+
+    internal class BotTestCase
+    {
+        public BotTestCase()
+        {
+            this.ErrorMessageHandler = DefaultErrorMessageHandler;
+        }
+
+        public string Action { get; internal set; }
+
+        public string ExpectedReply { get; internal set; }
+
+        public Func<string, string, string, string> ErrorMessageHandler { get; internal set; }
+
+        public Action<string> Verified { get; internal set; }
+
+        private static string DefaultErrorMessageHandler(string action, string expectedReply, string receivedReply)
+        {
+            return $"'{action}' received reply '{receivedReply}' that doesn't contain the expected message: '{expectedReply}'";
+        }
+    }
+}
